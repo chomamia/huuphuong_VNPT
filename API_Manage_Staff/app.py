30d9d31be55a5af1fe6e81d1c59411f4a0399a1a
chomamia <86954104+chomamia@users.noreply.github.com>
2022-07-17 16:33:46 +0700

    Delete API_Manage_Staff directory

--- API_Manage_Staff/app.py
+++
@@ -1,6 +0,0 @@
-from distutils.log import debug
-from library import create_app
-
-if __name__ == "__main__":
-    app = create_app()
-    app.run(debug = True)
--- API_Manage_Staff/library/library_ma.py
+++
@@ -1,6 +0,0 @@
-from dataclasses import field
-from .extension import ma
-
-class StaffSchema(ma.Schema):
-    class Meta:
-        fields = ('idNhanvien', 'tenNhanvien', 'CCCD', 'email', 'phone')
